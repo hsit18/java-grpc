5b87b37503d1332b0f0abfc23be3b81830adeb92
Singh, Harpreet (Harpreet Singh) <harpreets1@avaya.com>
2024-06-08 00:05:22 +0530

    GreetClient implemented

--- src/main/java/org/grpcdemo/client/GreetClient.java
+++ src/main/java/org/grpcdemo/client/GreetClient.java
@@ -1,4 +1,18 @@
 package org.grpcdemo.client;
 
+import io.grpc.ManagedChannelBuilder;
+import org.grpcdemo.proto.model.greet.GreetGrpc;
+import org.grpcdemo.proto.model.greet.GreetRequest;
+
 public class GreetClient {
+    private static int grpcPort = 9900;
+    public static void main(String[] args) {
+        System.out.println("Hello from GreetClient");
+        var channel = ManagedChannelBuilder.forAddress("localhost", grpcPort).usePlaintext().build();
+        var stub = GreetGrpc.newBlockingStub(channel);
+        var request = GreetRequest.newBuilder().setName("Harpreet").build();
+        var response = stub.greet(request);
+        System.out.println(response);
+
+    }
 }
--- src/main/java/org/grpcdemo/service/GreetService.java
+++ src/main/java/org/grpcdemo/service/GreetService.java
@@ -1,14 +1,18 @@
 package org.grpcdemo.service;
 
+import io.grpc.stub.StreamObserver;
 import org.grpcdemo.proto.model.greet.GreetGrpc;
 import org.grpcdemo.proto.model.greet.GreetRequest;
 import org.grpcdemo.proto.model.greet.GreetResponse;
 
 public class GreetService extends GreetGrpc.GreetImplBase {
-    public void Greet(GreetRequest request, GreetResponse responseObserver) {
+    @Override
+    public void greet(GreetRequest request, StreamObserver<GreetResponse> responseObserver) {
         System.out.println("Received request: " + request);
         var response = GreetResponse.newBuilder()
                 .setMessage("Hello how are you " + request.getName())
                 .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 }
